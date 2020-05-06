c3f33d24a721bb74b5440cbfbcf2726ddfcf63c1
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-06 18:43:11 +0900

    [add]comment-Asynchronous_communication

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,19 @@
 class BookCommentsController < ApplicationController
 	def create
+		@book_post = Book.find(params[:book_id])
+		@book_comment = BookComment.new
 		book = Book.find(params[:book_id])
 		comment = current_user.book_comments.new(book_comment_params)
 		comment.book_id = book.id
 		comment.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
+		@book_post = Book.find(params[:book_id])
 		book = Book.find(params[:book_id])
 		comment = current_user.book_comments.find_by(book_id: book.id)
 		comment.book_id = book.id
 		comment.destroy
-		redirect_back(fallback_location: root_path)
 	end
 
 	private
